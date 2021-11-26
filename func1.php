086e792e17818df6a2aa99f66b29ca6e1759b9ae
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-26 09:59:48 +0700

    update v1

--- func1.php
+++ func1.php
@@ -1,2 +1,33 @@
 <?php
-//Function
+//Function in PHP
+function add(){
+    $a = 10;
+    $b = 5;
+    $add = $a + $b;
+    echo "This is a Function add = " . $add ."<br />";
+}
+
+function sub(){
+    $a = 10;
+    $b = 5;
+    $sub = $a - $b;
+    echo"This is a Function Sub = ".$sub."<br />";
+}
+
+function division(){
+    $a = 10;
+    $b = 5;
+    $division = $a / $b;
+    echo"This is a Function division = ".$division."<br />";
+}
+
+function multiply(){
+    $a = 10;
+    $b = 5;
+    $multip = $a * $b;
+    echo"This is a Function multiply = ".$multip;
+}
+add();
+sub();
+division();
+multiply();
